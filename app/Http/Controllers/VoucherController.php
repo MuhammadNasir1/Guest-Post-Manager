097c28c46aea86a4f68aa06556e6a73f81113850
M-ArhamWaheed <=>
2024-06-29 14:56:00 +0500

    Add

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -10,26 +10,28 @@ class VoucherController extends Controller
 {
     public function addVoucher(Request $request)
     {
-        $validatedData = $request->validate([
-            "date" => "required",
-            "account" => "required",
-            "voucher_type" => "required",
-            "credit" => "nullable",
-            "debit" => "nullable",
-            "hint" => "nullable",
-        ]);
+        try {
+            $validatedData = $request->validate([
+                "date" => "required",
+                "account" => "required",
+                "voucher_type" => "required",
+                "hint" => "nullable",
+            ]);
 
-        $voucher = new Voucher;
-        $voucher->transaction_id = 1;
-        $voucher->date = $validatedData['date'];
-        $voucher->user_id = $validatedData['account'];
-        $voucher->voucher_type = $validatedData['voucher_type'];
-        $voucher->credit = $validatedData['credit'];
-        $voucher->debit = $validatedData['debit'];
-        $voucher->hint = $validatedData['hint'];
-        $voucher->save();
+            $voucher = new Voucher;
+            $voucher->transaction_id = 1;
+            $voucher->date = $validatedData['date'];
+            $voucher->user_id = $validatedData['account'];
+            $voucher->voucher_type = $validatedData['voucher_type'];
+            $voucher->credit = $request->credit;
+            $voucher->debit = $request->debit;
+            $voucher->hint = $validatedData['hint'];
+            $voucher->save();
 
-        return redirect('../transactionVoucher');
+            return redirect('../transactionVoucher');
+        } catch (\Exception $error) {
+            return response()->json(['error' => $error->getMessage()]);
+        }
     }
 
     public function getUser()
