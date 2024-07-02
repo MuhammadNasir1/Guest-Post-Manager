1f97d1fa34bc11db5684163007b6dea429aa912f
M-ArhamWaheed <=>
2024-07-02 12:09:09 +0500

    C3

--- resources/views/print_voucher.blade.php
+++ resources/views/print_voucher.blade.php
@@ -5,7 +5,7 @@
     <img src="{{ asset('../images/your-logo.jpg') }}" width="100" alt="">
     <div>
         <h1 class="pb-3 text-red-600 text-3xl font-bold">Your Company</h1>
-        <p class="ps-1">PH NO: 0303111111</p>
+        <p class="ps-1">@lang('lang.PH_NO') 0303111111</p>
     </div>
 </div>
 
@@ -14,20 +14,22 @@
 
 
         <div>
-            <h1 class="pb-1  text-2xl font-bold">Details</h1>
-            <h1 class="pb-2  text-3xl font-bold">Cash In Hand</h1>
+            <h1 class="pb-1  text-2xl font-bold">@lang('lang.Details')</h1>
+            <h1 class="pb-2  text-3xl font-bold">@lang('lang.Cash_In_Hand')</h1>
             <p class="ps-1">0</p>
         </div>
 
 
         <div>
-            <h2 class="pb-2.5 text-3xl font-bold">Voucher : <span class="font-bold ps-3">{{ $print->id }}</span>
+            <h2 class="pb-2.5 text-3xl font-bold">@lang('lang.Voucher') : <span
+                    class="font-bold ps-3">{{ $print->id }}</span>
             </h2>
-            <h2 class="pb-2.5  font-bold">Vendor Type : <span class="font-light ps-3">General Voucher</span> </h2>
-            <h2 class="pb-2.5  font-bold">Vendor Date/Time : <span class="font-light ps-3">Tue 29-August-2024 12:35
-                    <h2 class="pb-2.5  font-bold">Added By : <span class="font-light ps-3">Admin</span>
+            <h2 class="pb-2.5  font-bold">@lang('lang.Vendor_Type') : <span class="font-light ps-3">@lang('lang.General_Voucher')</span>
+            </h2>
+            <h2 class="pb-2.5  font-bold">@lang('lang.Vendor_Date_Time') : <span class="font-light ps-3">Tue 29-August-2024 12:35
+                    <h2 class="pb-2.5  font-bold">@lang('lang.Added_By') : <span class="font-light ps-3">Admin</span>
                     </h2>
-                    <h2 class="pb-2.5  font-bold">Type : <span class="font-light ps-3">Bank</span>
+                    <h2 class="pb-2.5  font-bold">@lang('lang.Type') : <span class="font-light ps-3">Bank</span>
                     </h2>
 
         </div>
@@ -39,15 +41,15 @@
 
             <body>
                 <tr>
-                    <th class="py-3 border px-5">Amount Paid</th>
+                    <th class="py-3 border px-5">@lang('lang.Amount_Paid')</th>
                     <td class="border px-5">5,000</td>
-                    <th class="border px-5">Previous Balance</th>
+                    <th class="border px-5">@lang('lang.Previous_Balance')</th>
                     <td class="border px-5">5,000</td>
-                    <th class="border px-5">Current Balance</th>
+                    <th class="border px-5">@lang('lang.Current_Balance')</th>
                     <td class="border px-5">5,000</td>
                 </tr>
                 <tr>
-                    <th class="py-3 px-5  border">Narration</th>
+                    <th class="py-3 px-5  border">@lang('lang.Narration')</th>
                     <td class="border px-5" colspan="5">{{ $print->hint }}</td>
                 </tr>
             </body>
@@ -60,16 +62,16 @@
         <div class="flex gap-3">
             {{-- <div class="w-[5px] h-[54px] bg-black mt-3"></div> --}}
             <div>
-                <h1 class="pb-2">Thank You So Much For Choosing <span class="text-2xl font-bold pt-2">Your
+                <h1 class="pb-2">@lang('lang.Thank_You_So_Much_For_Choosing') <span class="text-2xl font-bold pt-2">Your
                         Company</span>
                 </h1>
-                <h1 class="">Software Developed By <span class="text-2xl font-bold">The Web Concept</span></h1>
+                <h1 class="">@lang('lang.Software_Developed_By') <span class="text-2xl font-bold">The Web Concept</span></h1>
             </div>
         </div>
 
 
         <div>
-            <h2 class="pb-2.5 text-xl font-bold">Customer Copy</h2>
+            <h2 class="pb-2.5 text-xl font-bold">@lang('lang.Customer_Copy')</h2>
 
 
         </div>
@@ -79,11 +81,11 @@
     <div class="flex justify-between items-center mt-10 px-5">
 
         <div>
-            <h2 class="inline-block font-bold text-xl">Prepared By : _____________</h2>
+            <h2 class="inline-block font-bold text-xl">@lang('lang.Prepared_By') : _____________</h2>
         </div>
 
         <div>
-            <h2 class="inline-block font-bold text-xl">Recieved By : _____________</h2>
+            <h2 class="inline-block font-bold text-xl">@lang('lang.Recieved_By') : _____________</h2>
         </div>
 
     </div>
