592ffa808a6b35c8c96287494c34440af9ef5ec0
M-ArhamWaheed <=>
2024-07-15 13:07:05 +0500

    Add Record Page

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -311,4 +311,16 @@ return [
     "Select_Casino" => "Select Casino",
     "Guidelines" => "Guidelines",
     "Guidelines_Here" => "Guidelines Here",
+
+
+
+    "Record_List" => "Record List",
+    "Add_Record" => "Add Record",
+    "Client_From" => "Client From",
+    "Client_Name" => "Client Name",
+    "Client_Company" => "Client Company",
+    "Client_Email" => "Client Email",
+    "Client_Profile" => "Client Profile",
+    "Client_Conatact" => "Client Contact",
+
 ];
