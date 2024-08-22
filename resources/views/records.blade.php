a057e5f3c9b38c3d6c781f6d1322e05d1847ba3d
MuhammadNasir1 <nmian7080@gmail.com>
2024-08-22 18:32:11 +0500

    fix bug

--- resources/views/records.blade.php
+++ resources/views/records.blade.php
@@ -76,7 +76,7 @@
                                                 </svg>
                                             </button>
                                         </a>
-                                        <a href="" id="delLink">
+                                        <a href="../delRecord/{{ $data->id }}" id="delLink">
                                             <button class="delButton" delId="{{ $data->id }}">
                                                 <svg width="36" height="36" viewBox="0 0 36 36" fill="none"
                                                     xmlns="http://www.w3.org/2000/svg">
